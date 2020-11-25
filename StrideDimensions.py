5f14cb23cc53419c9b2ef6acaff2bdbdbcaf9e82
Helge Modén <helgemod@gmail.com>
2020-11-25 18:40:46 +0100

    Made variables internal to object.

--- StrideDimensions.py
+++ StrideDimensions.py
@@ -12,12 +12,12 @@
 #
 # Usage:
 #
-#   Fast example:
-#       Tic-Tac-Toe board   -> ticTacToeBoard = StrideDimension([3,3])
-#       Sudoku board        -> sudokuBoard = StrideDimension([3,3,9])
-#       Chessboard          -> chessBoard = StrideDimension([8,8])
+#   Popular examples:
+#       Tic-Tac-Toe board   -> ticTacToeBoard = StrideDimension((3,3))
+#       Sudoku board        -> sudokuBoard = StrideDimension((3,3,9))
+#       Chessboard          -> chessBoard = StrideDimension((8,8))
 #
-# Crate an object of class StrideDimension with values:
+# Create an object of class StrideDimension with values:
 # inDim -   a tuple with the dimensions that shall be represented.
 #           Example 1: To represent two dimension, eg. tic-tac-toe board
 #           use isDim = (3,3). This means two dimensions with 3 in each
@@ -41,38 +41,42 @@ __version__ = "1.0.1"
 __maintainer__ = "Helge Modén, https://github.com/helgemod/StrideDimensions"
 __email__ = "helgemod@gmail.com"
 __status__ = "https://github.com/helgemod/StrideDimensions"
-__date__ = "2020-11-17"
+__date__ = "2020-11-20"
 
 ########################################################################
 class StrideDimension:
 
 
     #Note, offset is not really used in this version.
-    offset = 0 #Offset where acual data starts in theData (Typically 0)
-    dimensions = []
-    strides = [] #How many jumps in "theData" to reach next data in that dimension.
-    theData = [] #The actual data
+    # offset = 0 #Offset where acual data starts in theData (Typically 0)
+    # dimensions = []
+    # strides = [] #How many jumps in "theData" to reach next data in that dimension.
+    # theData = [] #The actual data
 
     KEY_OFFSET      = "KEY_StrideDimensionOffset"
     KEY_DIMENSIONS  = "KEY_StrideDimensionDimensions"
     KEY_STRIDES     = "KEY_StrideDimensionStrides"
     KEY_THEDATA     = "KEY_StrideDimensionTheData"
 
-    def __init__(self,inDim,offset=0,fillWithNumbering=False):
+    def __init__(self, inDim, offset=0, fillWithNumbering= False):
+        self.offset = 0
+        self.dimensions = []
+        self.strides = [] #How many jumps in "theData" to reach next data in that dimension.
+        self.theData = [] #The actual data
 
         #Check valid data
-        if not (isinstance(inDim,list) or isinstance(inDim,tuple)):
-            inDim = [1,1]
+        if not (isinstance(inDim, list) or isinstance(inDim, tuple)):
+            inDim = [1, 1]
 
-        if not isinstance(fillWithNumbering,bool):
-            fillWithNumbering=False
+        if not isinstance(fillWithNumbering, bool):
+            fillWithNumbering = False
 
-        if not isinstance(offset,int) or offset<0:
-            offset=0
+        if not isinstance(offset, int) or offset < 0:
+            offset = 0
 
         #SETUP Data
-        self.offset=offset
-        self.dimensions = inDim
+        self.offset = offset
+        self.dimensions = list(inDim)
         dataLength = 1
         for element in inDim:
             dataLength *= element
@@ -84,9 +88,9 @@ class StrideDimension:
                 self.theData.append(None)
 
 
-        self.strides = [0]*len(inDim)
-        self.strides[0]=1 #Alwas one step between values in first dimension
-        for i in range(1,len(inDim)):
+        self.strides = [0] * len(inDim)
+        self.strides[0] = 1 #Alwas one step between values in first dimension
+        for i in range(1, len(inDim)):
             self.strides[i] = self.strides[i-1]*inDim[i-1]
 
     #For debug reason
@@ -116,18 +120,20 @@ class StrideDimension:
         return  {self.KEY_OFFSET:self.offset,self.KEY_DIMENSIONS:self.dimensions,self.KEY_STRIDES:self.strides,self.KEY_THEDATA:self.theData}
 
     #Set ONE value at coords (on format (2,3,4))
-    def setData(self,coords,value):
-        index = self.__indexForDimCoordinate(coords)
-        self.__setDataAtIndex(index,value)
+    def setData(self, coords, value):
+        index = self.indexForDimCoordinate(coords)
+        self.setDataAtIndex(index, value)
 
     #Get ONE value at coords (on format (2,3,4))
     #For multiple value, choose loopThruData or loopThruDimension below!
-    def getData(self,coords):
-        index = self.__indexForDimCoordinate(coords)
+    def getData(self, coords):
+        index = self.indexForDimCoordinate(coords)
         if index == None:
             return None
         return self.theData[index]
 
+    def getAllData(self):
+        return self.theData
 
     # def getDimensionalData(self,inDims):
     #
@@ -151,13 +157,13 @@ class StrideDimension:
     # Further examples:
     # To get all values in the middle square of a sudoku board call:
     #   getDimensionalData((None,None,5)) - Saying "loop through all x-data and y-data in dimension 5."
-    def getDimensionalData(self,inDims):
+    def getDimensionalData(self, inDims):
         #Sanity check
         if not len(inDims) == len(self.dimensions):
             return None
 
         if not None in inDims:
-            index = self.__indexForDimCoordinate(inDims)
+            index = self.indexForDimCoordinate(inDims)
             if index == None:
                 return None
             else:
@@ -193,13 +199,13 @@ class StrideDimension:
     #   getDimensionalDataWithDirection((3,3), (-1,-1)) will return [9,5,1]
     #
     def getDimensionalDataWithDirection(self, startCoord, direction):
-        startIndex = self.__indexForDimCoordinate(startCoord)
+        startIndex = self.indexForDimCoordinate(startCoord)
         if startIndex==None:
             return []
         retList = [self.theData[startIndex]]
-        nextCoord = self.__getNextCoordinateAfter(startCoord,direction)
-        while not nextCoord == None:
-            retList.append(self.theData[self.__indexForDimCoordinate(nextCoord)])
+        nextCoord = self.__getNextCoordinateAfter(startCoord, direction)
+        while nextCoord is not None:
+            retList.append(self.theData[self.indexForDimCoordinate(nextCoord)])
             nextCoord = self.__getNextCoordinateAfter(nextCoord, direction)
         return retList
 
@@ -211,13 +217,13 @@ class StrideDimension:
     #
     # Starts at the beginning of internal represenation of data
     # and returns index of first occurrence of "inData".
-    def getIndexAtFirstOccurrenceOfData(self,inData):
+    def getIndexAtFirstOccurrenceOfData(self, inData):
         for i in range(len(self.theData)):
             if self.theData[i] == inData:
                 return i
         return None
 
-    # Place data directly indexed - Use in conjunction with "getIndexAtFirstOccurrenceOfData"
+    # Place data directly indexed - Typical use in conjunction with "getIndexAtFirstOccurrenceOfData"
     def setDataAtIndex(self, index, value):
         if index == None or not isinstance(index,int):
             return
@@ -225,15 +231,101 @@ class StrideDimension:
             return
         self.theData[index] = value
 
+    def getIndexListWhereDataIs(self, compareData):
+        retList = []
+        for i in range(len(self.theData)):
+            if self.theData[i]==compareData:
+                retList.append(i)
+        return retList
+
+    def getCoordinatesForListIndex(self, index):
+        print("*** STUB *** <getCoordinatesForListIndex>")
+        return 0
+
     # Fill all data in all dimensions with "data"
     def fillData(self,data):
         mult=len(self.theData)
         self.theData = [data]*mult
 
+    # returns a tuple with the worlds coordinates
+    def dimCoordinateForIndex(self, index):
+        coords = []
+        if index >= len(self.theData):
+            return None
+        for x in range(len(self.dimensions)):
+            var = index // self.strides[x] % self.dimensions[x] + 1  # plus 1 because I don't like first dimension to be zero.
+            coords.append(var)
+        return coords
+
+    # dim is a tuple with the coordinates. Coordinate begins at 1 for each dimension. (1,1,1) is "Origo".
+    def indexForDimCoordinate(self, dim):
+        if not len(dim) == len(self.dimensions):
+            return None
+        index = 0
+        for x in range(len(dim)):
+            index += (dim[x] - 1) * self.strides[x]
+        if index < 0 or index >= len(self.theData):
+            return None
+        return index
+
+    def extendDimension(self, dimensionToExtend, byHowMuch, before, fillData=None):
+        ##############################
+        #        Sanity check
+        ##############################
+
+        if dimensionToExtend <= 0 or dimensionToExtend > len(self.dimensions):
+            raise Exception("*** No such dimension to extend ***")
+
+        ##############################
+        # Calculate start index
+        # for input of dimensions data
+        ##############################
+        if before:
+            startIndex = 0  # That's easy!
+        else:
+            if dimensionToExtend < len(self.strides):
+                startIndex = self.strides[dimensionToExtend]
+            else:
+                startIndex = len(self.theData)
+
+        ##############################
+        # Calculate how many places
+        # needed to insert data.
+        ##############################
+        noOfPlacesToInsertData = 1
+        if not dimensionToExtend == len(self.dimensions):
+            for i in range(dimensionToExtend, len(self.dimensions)):
+                noOfPlacesToInsertData *= self.dimensions[i]
 
 
+        ##############################
+        # Calculate how much data
+        # to insert.
+        ##############################
+        howMuchDataToInsert = byHowMuch * self.strides[dimensionToExtend-1]
 
+        ##############################
+        # Calculate how much is stride
+        # between each insertion.
+        ##############################
+        strideToNext = 0
+        if noOfPlacesToInsertData > 1:
+            strideToNext = self.strides[dimensionToExtend] + howMuchDataToInsert
 
+        insertData = [fillData]*howMuchDataToInsert
+        if noOfPlacesToInsertData <= 1:
+            self.theData[startIndex:startIndex] = insertData
+        else:
+            for i in range(noOfPlacesToInsertData):
+                self.theData[startIndex+(i*strideToNext):startIndex+(i*strideToNext)] = insertData
+
+        # Update dimensions information
+        self.dimensions[dimensionToExtend - 1] += byHowMuch
+
+        # Update strides information
+        self.strides[0] = 1  # Always one step between values in first dimension
+        for i in range(1, len(self.dimensions)):
+            self.strides[i] = self.strides[i - 1] * self.dimensions[i - 1]
 
 
 
@@ -243,24 +335,14 @@ class StrideDimension:
     #
     ########################################################
 
-    # dim is a tuple with the coordinates. Coordinate begins at 1 for each dimension. (1,1,1) is "Origo".
-    def __indexForDimCoordinate(self,dim):
-        if not len(dim)==len(self.dimensions):
-            return None
-        index = 0
-        for x in range(len(dim)):
-            index += (dim[x]-1) * self.strides[x]
-        if index<0 or index>=len(self.theData):
-            return None
-        return index
-
-    def __getNextCoordinateAfter(self,inCoord,dir):
+    def __getNextCoordinateAfter(self, inCoord, dir):
         nextCoord = []
-        #inIndex = self.__indexForDimCoordinate(inCoord)
         for x in range(len(inCoord)):
             nextCoord.append(inCoord[x] + dir[x])
         for y in range(len(self.dimensions)):
             if nextCoord[y] <= 0 or nextCoord[y] > self.dimensions[y]:
                 return None
-        #outIndex = self.__indexForDimCoordinate(nextCoord)
         return nextCoord
+
+if __name__ == '__main__':
+    pass
