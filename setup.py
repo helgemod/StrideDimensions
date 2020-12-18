59489ad63f327539e4317cd119a6d0ebc618392a
Helge Modén <helgemod@gmail.com>
2020-12-18 17:33:14 +0100

    Made a structure as a real python module.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='StrideDimensions',
+      version='0.1',
+      description='Python module that implements multidimensional data in one dimensional array using stride indexing.',
+      url='http://www.github.com/helgemod',
+      author='Helge Modén',
+      author_email='helgemod@gmail.com',
+      license='MIT',
+      packages=['StrideDimensions'],
+      zip_safe=False)
